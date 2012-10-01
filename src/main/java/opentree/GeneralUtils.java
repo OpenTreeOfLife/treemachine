1785928748aa6460f6588b007dd7ffbff32bae46
Joseph W. Brown <phylo.jwb@gmail.com>
2012-10-01 17:00:56 -0300

    comment

--- src/main/java/opentree/GeneralUtils.java
+++ src/main/java/opentree/GeneralUtils.java
@@ -2,6 +2,15 @@ package opentree;
 
 import java.util.*;
 
+/* 
+ * Is this used anywhere?
+ * 
+ * Is so, what are the properties of the list being passed in?
+ * 1. Is it guaranteed to start at 1 (or 0)?
+ * 2. Is it consecutive from start to length(list)?
+ * 
+ */
+
 public class GeneralUtils {
 
 	public static int sum_ints(List<Integer> list){
