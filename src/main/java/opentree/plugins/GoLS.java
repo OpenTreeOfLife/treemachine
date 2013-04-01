66ff2c44514c5ab37ff662d812714903da50c640
Mark T. Holder <mtholder@gmail.com>
2013-04-01 17:03:32 -0500

    start of graph of life query web services.  Compiles, but unchecked

---
+++ src/main/java/opentree/plugins/GoLS.java
@@ -0,0 +1,45 @@
+package opentree.plugins;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import opentree.GraphDatabaseAgent; 
+
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.server.plugins.Description;
+import org.neo4j.server.plugins.Parameter;
+import org.neo4j.server.plugins.PluginTarget;
+import org.neo4j.server.plugins.ServerPlugin;
+import org.neo4j.server.plugins.Source;
+import org.neo4j.server.rest.repr.Representation;
+import org.neo4j.server.rest.repr.OpenTreeMachineRepresentationConverter;
+
+// Graph of Life Services 
+public class GoLS extends ServerPlugin {
+
+	@Description("Returns a list of all source tree IDs")
+	@PluginTarget(GraphDatabaseService.class)
+	public Representation getSourceTreeIDs(
+			@Source GraphDatabaseService graphDb) {
+		GraphDatabaseAgent gdb = new GraphDatabaseAgent(graphDb);
+		Index<Node> sourceRootIndex = gdb.getNodeIndex("sourceRootNodes");
+		int results = 0;
+		for (Node rootNd : sourceRootIndex.query("rootnode", "*")) {
+			results += 1;
+		}
+		gdb.shutdownDb();
+		return OpenTreeMachineRepresentationConverter.convert(results);
+	}
+}
