fbe2793a26b52be6c06a67f2cc44c1dc73abe30a
Stephen Smith <blackrim@gmail.com>
2013-05-08 09:11:26 -0400

    added synth list

--- src/main/java/opentree/MainRunner.java
+++ src/main/java/opentree/MainRunner.java
@@ -834,6 +834,38 @@ public class MainRunner {
 	}
 	
 	/// @returns 0 for success, 1 for poorly formed command, -1 for failure
+		public int synthesizeDraftTreeWithList(String [] args) throws OttolIdNotFoundException {
+			if (args.length != 4) {
+				System.out.println("arguments should be rootOTToLid listofsources(CSV) graphdbfolder");
+				return 1;
+			}
+			String ottolId = args[1];
+			String slist = args[2];
+			String graphname = args[3];
+			boolean success = false;
+			GraphExplorer ge = new GraphExplorer(graphname);
+			try {
+				// build the list of preferred sources, this should probably be done externally
+				LinkedList<String> preferredSources = new LinkedList<String>();
+				String [] tsl = slist.split(",");
+				for(int i=0;i<tsl.length;i++){preferredSources.add(tsl[i]);}
+				preferredSources.add("taxonomy");
+				
+				// find the start node
+				Node firstNode = ge.findGraphTaxNodeByUID(ottolId);
+				if (firstNode == null) {
+					throw new opentree.OttolIdNotFoundException(ottolId);
+				}
+				success = ge.synthesizeAndStoreDraftTreeBranches(firstNode, preferredSources);
+			} catch (OttolIdNotFoundException oex) {
+				oex.printStackTrace();
+			} finally {
+				ge.shutdownDB();
+			}
+			return (success ? 0 : -1);
+		}
+	
+	/// @returns 0 for success, 1 for poorly formed command, -1 for failure
 	public int synthesizeDraftTree(String [] args) throws OttolIdNotFoundException {
 		if (args.length != 3) {
 			System.out.println("arguments should be rootOTToLid graphdbfolder");
@@ -1142,6 +1174,7 @@ public class MainRunner {
 
 		System.out.println("---synthesis functions---");
 		System.out.println("\tsynthesizedrafttree <rootNodeId> <graphdbfolder> (perform default synthesis from the root node using source-preference tie breaking and store the synthesized rels)");
+		System.out.println("\tsynthesizedrafttreelist <rootNodeId> <graphdbfolder> (perform default synthesis from the root node using source-preferenc tie breaking and store the synthesized rels with a list (csv))");
 		System.out.println("\textractdrafttree <rootNodeId> <outfilename> <graphdbfolder> extracts the default synthesized tree (if any) stored below the root node\n");
 				
 		System.out.println("---temporary functions---");
