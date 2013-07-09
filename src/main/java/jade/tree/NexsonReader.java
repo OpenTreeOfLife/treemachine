15b41f9459137c2345b24b314b79a419ab20ff1f
Mark T. Holder <mtholder@gmail.com>
2013-07-09 16:45:53 -0500

    suprressing an unhelpful warning

--- src/main/java/jade/tree/NexsonReader.java
+++ src/main/java/jade/tree/NexsonReader.java
@@ -236,6 +236,8 @@ public class NexsonReader {
 								System.err.println("Error with: " + m);
 								throw new RuntimeException("Invalid ottolid value: " + value);
 							}
+						} else if(propname.equals("ot:originalLabel")){
+							// ignoring originalLabel, but not emitting the unknown property warning
 						} else {
 							msgLogger.indentMessageStrStrStrStr(1, "Warning: dealing with unknown property. Don't know what to do...", "property name", propname, "nexsonid", id);
 						}
