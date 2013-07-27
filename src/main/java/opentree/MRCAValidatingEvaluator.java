4d72e0dfcea734577c6c88f4bfeee1f2a52c8c7d
Cody Hinchliff <cody.hinchliff@gmail.com>
2013-07-27 16:50:49 -0400

    needed for proper tree loading

---
+++ src/main/java/opentree/MRCAValidatingEvaluator.java
@@ -0,0 +1,62 @@
+package opentree;
+
+import gnu.trove.list.array.TLongArrayList;
+
+import opentree.constants.RelType;
+
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.traversal.Evaluation;
+import org.neo4j.graphdb.traversal.Evaluator;
+
+/**
+ * Neo4j Traversal Evaluator that checks nodes to make sure their mrca and outmrca properties are set. Nodes that need
+ * these properties to be updated will be returned as part of the path.
+ */
+public class MRCAValidatingEvaluator implements Evaluator {
+
+	TLongArrayList mrcaTest;
+	TLongArrayList outmrcaTest;
+
+	/**
+	 * Set the mrca and outmrca properties that we will use to validate nodes in the traversal. Any nodes we
+	 * visit that do not contain all the ids of the passed mrca and outmrca variables in the correspding properties
+	 * will be returned by the traversal.
+	 * @param mrca
+	 * @param outmrca
+	 */
+	public MRCAValidatingEvaluator(TLongArrayList mrca, TLongArrayList outmrca) {
+
+		this.mrcaTest = mrca;
+		this.outmrcaTest = mrca;
+
+	}
+
+	@Override
+	public Evaluation evaluate(Path inPath) {
+
+		Node curNode = inPath.endNode();
+
+		// pass over taxonomy nodes, their mrca never changes and they don't have an 'outmrca' field 
+		if (curNode.hasProperty("name")) {
+			return Evaluation.EXCLUDE_AND_CONTINUE;
+		}
+		
+		// include the node if it is missing any mrca ids
+		TLongArrayList curNodeDescendantIds = new TLongArrayList((long[]) curNode.getProperty("mrca"));
+		if (!curNodeDescendantIds.containsAll(mrcaTest)) {
+			return Evaluation.INCLUDE_AND_CONTINUE;
+		}
+
+		// return the node if it is missing any outmrca ids
+		TLongArrayList curNodeOutgroupIds = new TLongArrayList((long[]) curNode.getProperty("outmrca"));
+		if (!curNodeOutgroupIds.containsAll(outmrcaTest)) {
+			return Evaluation.INCLUDE_AND_CONTINUE;
+		}
+
+		// if we passed the tests, this node has all the right descendant ids, so we won't include it
+		return Evaluation.EXCLUDE_AND_PRUNE;
+	}
+}
