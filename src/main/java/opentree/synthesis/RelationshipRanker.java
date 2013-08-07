c93a327b309c1ee75376c0cb419df4c537467fc2
Joseph W. Brown <phylo.jwb@gmail.com>
2013-08-07 16:36:39 -0400

    less verbosity in ranking

--- src/main/java/opentree/synthesis/RelationshipRanker.java
+++ src/main/java/opentree/synthesis/RelationshipRanker.java
@@ -59,29 +59,29 @@ public class RelationshipRanker {
 		
 		
 		if (rankedRels.size() == 0) {
-			System.out.println("\n\t*** Whoops?!? No relationships to deal with! ***");
+		//	System.out.println("\n\t*** Whoops?!? No relationships to deal with! ***");
 		} else {
 			
-			System.out.println("\n*** Relationships BEFORE ranking: ***");
-			for (int i = 0; i < rankedRels.size(); i++) {
-				System.out.println("Relationship: " + rankedRels.get(i).toString() + "; Source: " + rankedRels.get(i).getProperty("source"));
-			}
-			System.out.print("\n");
+		//	System.out.println("\n*** Relationships BEFORE ranking: ***");
+		//	for (int i = 0; i < rankedRels.size(); i++) {
+		//		System.out.println("Relationship: " + rankedRels.get(i).toString() + "; Source: " + rankedRels.get(i).getProperty("source"));
+		//	}
+		//	System.out.print("\n");
 			
 			for (int i = criteria.size() - 1; i >= 0; i--) {
 				// iterate through criteria in reverse order and sort the relationships in that order
 				criteria.get(i).sort(rankedRels); // TEST THIS
 			}
 			
-			System.out.println("*** Relationships AFTER ranking: ***");
-			for (int i = 0; i < rankedRels.size(); i++) {
-				System.out.println("Relationship: " + rankedRels.get(i).toString() + "; Source: " + rankedRels.get(i).getProperty("source"));
-			}
-			System.out.print("\n");
+		//	System.out.println("*** Relationships AFTER ranking: ***");
+		//	for (int i = 0; i < rankedRels.size(); i++) {
+		//		System.out.println("Relationship: " + rankedRels.get(i).toString() + "; Source: " + rankedRels.get(i).getProperty("source"));
+		//	}
+		//	System.out.print("\n");
 			
-			if (!origRels.equals(rankedRels)) {
-				System.out.println("\n\t*** RANKING DIFFERS! ***\n");
-			}
+		//	if (!origRels.equals(rankedRels)) {
+		//		System.out.println("\n\t*** RANKING DIFFERS! ***\n");
+		//	}
 		}
 		
 		
