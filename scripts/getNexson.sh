de5d2a5c921f359dfe43d8a076d6b4282fee2b81
josephwb <phylo.jwb@gmail.com>
2013-12-03 12:56:23 -0500

    usage for getNexson.sh script

--- scripts/getNexson.sh
+++ scripts/getNexson.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+## script to grab individual nexson(s) from Phylografter
+## Usage:
+##     ./getNexson.sh study1ID study2ID ...
+## where study*ID is the Phylografter study identifier. For example, to get studies 420 and 1428, type:
+##     ./getNexson.sh 1428 420
+## It may be necessary to change access permission to make the script executable. Type:
+##     chmod 755 getNexson.sh
+
 if [ "$1" == "" ]
     then
         printf "Usage: \n./getNexson.sh study1 study2 ...\n"
