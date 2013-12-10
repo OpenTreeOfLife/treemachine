571c3955d1abf79d035094405bd167710f225bbc
josephwb <phylo.jwb@gmail.com>
2013-12-10 10:52:49 -0500

    minor

--- src/main/java/opentree/GeneralUtils.java
+++ src/main/java/opentree/GeneralUtils.java
@@ -19,30 +19,25 @@ public class GeneralUtils {
     public static final String offendingChars = "[\\Q\"_~`:;/[]{}|<>,.!@#$%^&*()?+=`\\\\\\E\\s]+";
 
     public static int sum_ints(List<Integer> list){
-		if(list==null || list.size()<1)
+		if (list == null || list.size() < 1) {
 			return 0;
+		}
 
 		int sum = 0;
-		for(Integer i: list)
-			sum = sum+i;
-
+		for (Integer i: list) {
+			sum = sum + i;
+		}
 		return sum;
 	}
 
 	/**
 	 * Replaces non-alphanumeric characters (excluding "_" and "-") in `dirtyName` with "_" and returns the cleaned name.
-	 * Currently slow and crappy, should be updated to use regex and just do a single pass over the string.
 	 * 
 	 * @param dirtyName
 	 * @return cleaned name
 	 */
 	public static String cleanName(String dirtyName) {
 	    String cleanName = dirtyName.replaceAll(offendingChars, "_");
-//	    for (char bad : badchars.toCharArray()) {
-//	        cleanName.replace(bad, '_');
-//	    }
-	    
-	    
 	    return cleanName;
 	}
 }
