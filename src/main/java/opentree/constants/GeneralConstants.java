59d2e47857fe94e814a4ec032b207740286ee903
josephwb <phylo.jwb@gmail.com>
2013-12-12 10:06:57 -0500

    comment

--- src/main/java/opentree/constants/GeneralConstants.java
+++ src/main/java/opentree/constants/GeneralConstants.java
@@ -10,8 +10,10 @@ package opentree.constants;
  *
  */
 public enum GeneralConstants {
-
-	DRAFT_TREE_NAME (String.class, "otol.draft.22"); // this should not be a constant, it should be set by the user when making the draft tree. it's just here for now as a convenience
+	
+	// this should not be a constant, it should be set by the user when making the draft tree. it's just here for now as a convenience
+	// agreed -- we may want to have several different draft trees in the db at a time. 
+	DRAFT_TREE_NAME (String.class, "otol.draft.22");
 
     public final Class<?> type;
     public final Object value;
