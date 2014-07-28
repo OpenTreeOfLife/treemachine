421c3adf8e76b610abb782dab4d79eddb3f51c50
josephwb <phylo.jwb@gmail.com>
2014-07-28 15:29:12 -0400

    minor formatting

--- src/main/java/opentree/GraphDatabaseAgent.java
+++ src/main/java/opentree/GraphDatabaseAgent.java
@@ -19,149 +19,155 @@ import org.neo4j.kernel.EmbeddedGraphDatabase;
  */
 public class GraphDatabaseAgent {
 
-    /*
-    private static EmbeddedGraphDatabase embeddedGraphDb;
-    private static GraphDatabaseService graphDbService;
-    private static boolean embedded;
-    */
-    
-    private EmbeddedGraphDatabase embeddedGraphDb;
-    private GraphDatabaseService graphDbService;
-    private boolean embedded;
-
-    public GraphDatabaseAgent(GraphDatabaseService gdbs) {
-        graphDbService = gdbs;
-        embedded = false;
-    }
-
-    public GraphDatabaseAgent(EmbeddedGraphDatabase egdb) {
-        embeddedGraphDb = egdb;
-        embedded = true;
-    }
-
-    public GraphDatabaseAgent(String graphDbName) {
-        embeddedGraphDb = new EmbeddedGraphDatabase(graphDbName);
-        embedded = true;
-    }
-
-    public Index<Node> getNodeIndex(String indexName) {
-        Index<Node> index; 
-        Map<String,String> indexPars = MapUtil.stringMap( "type", "exact", "to_lower_case", "true" );
-
-        if (embedded)
-            index = embeddedGraphDb.index().forNodes(indexName, indexPars);
-        else
-            index = graphDbService.index().forNodes(indexName, indexPars);
-        
-        return index;
-    }
-
-    public Index<Relationship> getRelIndex(String indexName) {
-        Index<Relationship> index; 
-        Map<String,String> indexPars = MapUtil.stringMap( "type", "exact", "to_lower_case", "true" );
-
-        if (embedded)
-            index = embeddedGraphDb.index().forRelationships(indexName, indexPars);
-        else
-            index = graphDbService.index().forRelationships(indexName, indexPars);
-        
-        return index;
-    }
-    
-    public Node createNode() {
-        if (embedded)
-            return embeddedGraphDb.createNode();
-        else
-            return graphDbService.createNode();
-    }
-
-    public Transaction beginTx() {
-        if (embedded)
-            return embeddedGraphDb.beginTx();
-        else
-            return graphDbService.beginTx();
-    }
-    
-    public Node getNodeById(Long arg0) {
-        if (embedded)
-            return embeddedGraphDb.getNodeById(arg0);
-        else
-            return graphDbService.getNodeById(arg0);
-    }
-
-    public Relationship getRelationshipById(Long arg0) {
-        if (embedded)
-            return embeddedGraphDb.getRelationshipById(arg0);
-        else
-            return graphDbService.getRelationshipById(arg0);
-    }
-
-    public void shutdownDb() {
-        registerShutdownHook();
-    }
-
-    /**
-     * These assume that the graph properties will be stored at the 
-     * root node as properties. The root node should always be node 0
-     * @param propname
-     * @return
-     */
-    public Object getGraphProperty(String propname){
-    	if(embedded){
-    		if (embeddedGraphDb.getNodeById(0).hasProperty(propname))
-    			return embeddedGraphDb.getNodeById(0).getProperty(propname);
-    		else
-    			return null;
-    	}else{
-    		if (graphDbService.getNodeById(0).hasProperty(propname))
-    			return graphDbService.getNodeById(0).getProperty(propname);
-    		else
-    			return null; 	    		
-    	}
-    }
-    
-    /**
-     * These assume that the graph properties will be stored at the actual graph db root node as properties. The root node of the
-     * graph db itself should always be node 0, although this is not necessarily of the tree alignment graph stored within the graphdb.
-     * @param propname, prop
-     * @return
-     */
-    public void setGraphProperty(String propname, Object prop){
-    	if(embedded){
-    		Transaction tx = embeddedGraphDb.beginTx();
-    		try{
-    			embeddedGraphDb.getNodeById(0).setProperty(propname,prop);
-    			tx.success();
-    		}finally{
-    			tx.finish();
-    		}
-    	}else{
-    		Transaction tx = graphDbService.beginTx();
-    		try{
-    			graphDbService.getNodeById(0).setProperty(propname,prop);
-    			tx.success();
-    		}finally{
-    			tx.finish();
-    		}
-    	}
-    }
-    
-    protected /*static */ void registerShutdownHook() {
-    	
-    	
-        if (embedded) {
-            embeddedGraphDb.shutdown();
-
-    	// Apparently the use of an anonymous instance of Thread.class is required when the graph is running as a service (at least on localhost).
-    	// If we just call the shutdown() method directly from within a plugin (via GraphExplorer.shutdownDB() for instance), the server fails.
-        } else {
-    	    Runtime.getRuntime().addShutdownHook(new Thread() {
+	/*
+	private static EmbeddedGraphDatabase embeddedGraphDb;
+	private static GraphDatabaseService graphDbService;
+	private static boolean embedded;
+	*/
 	
-	            @Override
-	            public void run() {
-	            	graphDbService.shutdown();
-	           }
-	        });
-        }
-    }
+	private EmbeddedGraphDatabase embeddedGraphDb;
+	private GraphDatabaseService graphDbService;
+	private boolean embedded;
+
+	public GraphDatabaseAgent(GraphDatabaseService gdbs) {
+		graphDbService = gdbs;
+		embedded = false;
+	}
+
+	public GraphDatabaseAgent(EmbeddedGraphDatabase egdb) {
+		embeddedGraphDb = egdb;
+		embedded = true;
+	}
+
+	public GraphDatabaseAgent(String graphDbName) {
+		embeddedGraphDb = new EmbeddedGraphDatabase(graphDbName);
+		embedded = true;
+	}
+
+	public Index<Node> getNodeIndex(String indexName) {
+		Index<Node> index; 
+		Map<String,String> indexPars = MapUtil.stringMap( "type", "exact", "to_lower_case", "true" );
+
+		if (embedded) {
+			index = embeddedGraphDb.index().forNodes(indexName, indexPars);
+		} else {
+			index = graphDbService.index().forNodes(indexName, indexPars);
+		}
+		return index;
+	}
+
+	public Index<Relationship> getRelIndex(String indexName) {
+		Index<Relationship> index; 
+		Map<String,String> indexPars = MapUtil.stringMap( "type", "exact", "to_lower_case", "true" );
+
+		if (embedded) {
+			index = embeddedGraphDb.index().forRelationships(indexName, indexPars);
+		} else {
+			index = graphDbService.index().forRelationships(indexName, indexPars);
+		}
+		return index;
+	}
+	
+	public Node createNode() {
+		if (embedded) {
+			return embeddedGraphDb.createNode();
+		} else {
+			return graphDbService.createNode();
+		}
+	}
+
+	public Transaction beginTx() {
+		if (embedded) {
+			return embeddedGraphDb.beginTx();
+		} else {
+			return graphDbService.beginTx();
+		}
+	}
+	
+	public Node getNodeById(Long arg0) {
+		if (embedded) {
+			return embeddedGraphDb.getNodeById(arg0);
+		} else {
+			return graphDbService.getNodeById(arg0);
+		}
+	}
+
+	public Relationship getRelationshipById(Long arg0) {
+		if (embedded) {
+			return embeddedGraphDb.getRelationshipById(arg0);
+		} else {
+			return graphDbService.getRelationshipById(arg0);
+		}
+	}
+
+	public void shutdownDb() {
+		registerShutdownHook();
+	}
+
+	/**
+	 * These assume that the graph properties will be stored at the 
+	 * root node as properties. The root node should always be node 0
+	 * @param propname
+	 * @return
+	 */
+	public Object getGraphProperty(String propname){
+		if (embedded) {
+			if (embeddedGraphDb.getNodeById(0).hasProperty(propname)) {
+				return embeddedGraphDb.getNodeById(0).getProperty(propname);
+			} else {
+				return null;
+			}
+		} else {
+			if (graphDbService.getNodeById(0).hasProperty(propname)) {
+				return graphDbService.getNodeById(0).getProperty(propname);
+			} else {
+				return null;
+			}
+		}
+	}
+	
+	/**
+	 * These assume that the graph properties will be stored at the actual graph db root node as properties. The root node of the
+	 * graph db itself should always be node 0, although this is not necessarily of the tree alignment graph stored within the graphdb.
+	 * @param propname, prop
+	 * @return
+	 */
+	public void setGraphProperty(String propname, Object prop){
+		if (embedded) {
+			Transaction tx = embeddedGraphDb.beginTx();
+			try {
+				embeddedGraphDb.getNodeById(0).setProperty(propname,prop);
+				tx.success();
+			} finally {
+				tx.finish();
+			}
+		} else {
+			Transaction tx = graphDbService.beginTx();
+			try {
+				graphDbService.getNodeById(0).setProperty(propname,prop);
+				tx.success();
+			} finally {
+				tx.finish();
+			}
+		}
+	}
+	
+	protected /*static */ void registerShutdownHook() {
+		
+		
+		if (embedded) {
+			embeddedGraphDb.shutdown();
+
+		// Apparently the use of an anonymous instance of Thread.class is required when the graph is running as a service (at least on localhost).
+		// If we just call the shutdown() method directly from within a plugin (via GraphExplorer.shutdownDB() for instance), the server fails.
+		} else {
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+	
+				@Override
+				public void run() {
+					graphDbService.shutdown();
+			   }
+			});
+		}
+	}
 }
