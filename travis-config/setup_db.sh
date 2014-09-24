aa3585a4068e59b6a12d43828d19af1121cdcdca
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-09-24 17:23:20 -0400

    adding asterales example

--- travis-config/setup_db.sh
+++ travis-config/setup_db.sh
@@ -1,10 +1,10 @@
-# install graph commander repo
+# first record the location of the treemachine standalone jar
+TREEMACHINE_JAR=$(pwd)'/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar' && export TREEMACHINE_JAR
+
+# now install graph commander repo and run the asterales example
 git clone http://github.com/OpenTreeOfLife/gcmdr.git
 cd gcmdr
-
-# run the asterales example
-TREEMACHINE_JAR=$(pwd)'/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar' && export TREEMACHINE_JAR
-./run_asterales_example.py
+python run_asterales_example.py
 cd ../
 
 # download neo4j
@@ -12,4 +12,5 @@ wget "http://neo4j.com/artifact.php?name=neo4j-community-1.9.8-unix.tar.gz"
 tar -xvf "artifact.php?name=neo4j-community-1.9.8-unix.tar.gz"
 mv neo4j-community-1.9.8 neo4j-server
 
-mv gcmdr/example/asterales_synth.db data/graph.db
+# move the asterales example to the default neo4j db location
+mv gcmdr/example/asterales_synth.db neo4j-server/data/graph.db
