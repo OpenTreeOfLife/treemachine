6b80b3a8576a258aca141ecd6d0b4fe1cfe4aa31
Stephen Smith <blackrim@gmail.com>
2015-03-02 21:30:12 -0500

    subsetting code

--- src/main/java/org/opentree/tag/treeimport/SubsetTreesUtility.java
+++ src/main/java/org/opentree/tag/treeimport/SubsetTreesUtility.java
@@ -2,6 +2,7 @@ package org.opentree.tag.treeimport;
 
 import jade.tree.Tree;
 import jade.tree.TreeNode;
+import jade.tree.JadeNode;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -58,20 +59,55 @@ public class SubsetTreesUtility {
 			TreeNode pruned = null;
 			CompactLongSet rootcls = getFullMRCAForNode(tr.getRoot());
 			if(subset_mrcas.containsAll(rootcls)== false && subset_mrcas.containsAny(rootcls) ){
-				System.out.println("subsetting:"+tr);
+				boolean external = false;
+				TreeNode nonmono = null;
+
+				//checks internal nodes
 				for(TreeNode in: tr.internalNodes(jade.tree.NodeOrder.POSTORDER)){
 					if(in == tr.getRoot())
 						continue;
 					CompactLongSet ts = getFullMRCAForNode(in);
 					CompactLongSet other = new CompactLongSet(rootcls);
 					other.removeAll(ts);
-					if(subset_mrcas.containsAll(ts) == false && subset_mrcas.containsAny(other) == false){
+					if(subset_mrcas.containsAll(ts) == true && subset_mrcas.containsAny(other) == false){
 						pruned = in;
 						//it will just be replaced as it goes down
+					}if(subset_mrcas.containsAll(ts) == false && subset_mrcas.containsAny(other) == false){
+						if(nonmono == null){
+							//if nonmono, set the first one, this is the mrca and will be used if not 
+							//    external and no internal node
+							nonmono = in;
+						}
 					}
 				}
-				//if we prune, then we need to go ahead and make the pruned tree for the next analysis
+				//if null, it could be external node (not a subset) or if non monophyletic, we want the mrca (including the nonmonophyly
+				if(pruned == null){
+					for(TreeNode ex: tr.externalNodes()){
+						CompactLongSet ts = getFullMRCAForNode(ex);
+						CompactLongSet other = new CompactLongSet(rootcls);
+						other.removeAll(ts);
+						if(subset_mrcas.containsAll(ts) == true && subset_mrcas.containsAny(other) == false){
+							external = true;
+							break;
+							//it will just be replaced as it goes down
+						}
+					}
+				}
 				
+				//if we prune, then we need to go ahead and make the pruned tree for the next analysis
+				if(external == false){
+					if(pruned == null)
+						pruned = nonmono;
+					System.out.println("pruned:"+pruned+"subset=1");
+					TreeNode par = pruned.getParent();
+					par.removeChild(pruned);
+					TreeNode nn = new JadeNode();
+					((JadeNode)nn).setName(taxId+"__subset=1");
+					par.addChild(nn);
+					System.out.println("subsetting:"+tr);
+				}else{
+					System.out.println("not included within:"+tr);
+				}
 			}else if(subset_mrcas.containsAny(rootcls) ==false){
 				System.out.println("not included within:"+tr);
 			}else if(subset_mrcas.containsAll(rootcls)== true){
