bbe50f2fd679a5a56a9da41f21f9faf0db21c785
josephwb <phylo.jwb@gmail.com>
2015-08-27 08:30:57 -0500

    better casing

--- src/main/java/org/neo4j/server/rest/repr/ArgusonRepresentationConverter.java
+++ src/main/java/org/neo4j/server/rest/repr/ArgusonRepresentationConverter.java
@@ -98,7 +98,7 @@ public class ArgusonRepresentationConverter extends MappingRepresentation {
 				}
 				
 				if (inNode.getObject("tip_descendants") != null) {
-					serializer.putNumber("tip_descendants", (Integer) inNode.getObject("tip_descendants"));
+					serializer.putNumber("nTipDescendants", (Integer) inNode.getObject("tip_descendants"));
 				}
 				
 				if (inNode.isInternal()) {
